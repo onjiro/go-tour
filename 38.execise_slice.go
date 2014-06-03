fd180b7e5a45f9fcfa6e94f8073344a0131278fc
onjiro <onjiro.mohyahya@gmail.com>
2014-06-03 10:54:05 +0900

    38 Exercise: Slices - implement x*y

--- 38.execise_slice.go
+++ 38.execise_slice.go
@@ -3,6 +3,14 @@ package main
 import "code.google.com/p/go-tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	ret := make([][]uint8, dy)
+	for y := range ret {
+		ret[y] = make([]uint8, dx)
+		for x := range ret[y] {
+			ret[y][x] = (uint8)(x * y)
+		}
+	}
+	return ret
 }
 
 func main() {
