18eb06f2349c7a495e3def0bc4eacd80aada5edd
onjiro <onjiro.mohyahya@gmail.com>
2014-06-04 13:33:59 +0900

    50 Advanced Exercise: Complex cube root - implemented

--- 50.advanced_exercise_complex_cube_root.go
+++ 50.advanced_exercise_complex_cube_root.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 )
 
 func Cbrt(x complex128) complex128 {
-	// todo
+	z := 1.0 + 0i
+	for i := 0; i < 100; i++ {
+		z = z - (cmplx.Pow(z, 3)-x)/(3*cmplx.Pow(x, 2))
+	}
+	return z
 }
 
 func main() {
