9a1378b6c60b766b32abc5bedb66dca7dda4bf79
onjiro <onjiro.mohyahya@gmail.com>
2014-06-06 10:41:00 +0900

    60 Exercise: HTTP Handlers - done

--- 60.exercise_http_handlers.go
+++ 60.exercise_http_handlers.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+type String string
+
+func (s String) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, s)
+}
+
+type Struct struct {
+	Greeting string
+	Punct    string
+	Who      string
+}
+
+func (s Struct) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, s.Greeting, s.Punct, s.Who)
+}
+
 func main() {
-	// your http.Handle calls here
-	http.ListenAndServe("localhost:4000", nil)
+	//s := String("I'm frayed knot.")
+	s := &Struct{"Hello", ":", "Gophers!"}
+	http.ListenAndServe("localhost:4000", s)
 }
