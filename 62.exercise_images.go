49d34ac253b0e579746d4c6da03e20e239255857
onjiro <onjiro.mohyahya@gmail.com>
2014-06-06 13:11:10 +0900

    62 Exercise: Images - done

--- 62.exercise_images.go
+++ 62.exercise_images.go
@@ -3,11 +3,27 @@ package main
 import (
 	"code.google.com/p/go-tour/pic"
 	"image"
+	"image/color"
 )
 
-type Image struct{}
+type Image struct {
+	Rectangle image.Rectangle
+}
+
+func (i *Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i *Image) Bounds() image.Rectangle {
+	return i.Rectangle
+}
+
+func (i *Image) At(x, y int) color.Color {
+	v := uint8(x * y)
+	return &color.RGBA{v, v, 255, 255}
+}
 
 func main() {
-	m := Image()
-	pic.ShowImage(m)
+	m := Image{image.Rect(0, 0, 100, 100)}
+	pic.ShowImage(&m)
 }
